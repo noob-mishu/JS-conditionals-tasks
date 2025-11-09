9c6b2c9b0b8be8a71fe29e1632f3639a95e1ab9c
Mishu Barua <mishubarua320@gmail.com>
2025-11-09 21:43:37 +0600

    Refactor comments and add Coke pricing logic

--- 1 Free Drinks.js
+++ 1 Free Drinks.js
@@ -4,3 +4,12 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+if(burgerPrice<500)
+{
+    console.log("Coke Is Free For You");
+}
+
+else
+{
+    console.log("You have to pay 30tk extra for coke");
+}
